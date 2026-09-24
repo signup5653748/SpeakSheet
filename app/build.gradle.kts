85432da98787656a4f773655e4cedce9c92f4989
signup5653748 <sign.up.email.466+temporary@gmail.com>
2026-09-24 12:19:23 +0530

    build: update build configuration and app metadata
    - Update JDK version to 21 for CI/CD
    - Set default compileSdk fallback to 36
    - Standardize compileSdk syntax in Gradle
    - Rename app release title to SpeakSheet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
   namespace = "com.speaksheet"
-  compileSdk { version = release(36) { minorApiLevel = 1 } }
+  compileSdk = 36
 
   defaultConfig {
     applicationId = "com.speaksheet"
